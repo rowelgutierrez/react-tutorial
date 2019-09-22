391c6ac90e5b8c59a42c2dc2d0ad5becb79a0b4f
rowel <rowel@elephantventures.com>
2019-09-22 16:13:12 +0800

    project sample

---
+++ berger-keng/src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from './Layout.module.css'
+
+const layout = (props) => (
+    <React.Fragment>
+        <div>
+            Toolbar
+            SideDrawer
+            Backdrop
+        </div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </React.Fragment>
+);
+
+export default layout;
